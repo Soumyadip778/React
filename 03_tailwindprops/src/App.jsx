047e68bcffaa467d02b2b26a0639716e25aff690
Soumyadip Maity <maitysoumya904@gamil.com>
2026-07-09 18:17:52 +0530

    New module

---
+++ 03_tailwindprops/src/App.jsx
@@ -0,0 +1,16 @@
+ 
+import { useState } from 'react'
+import './App.css'
+import Card from './components/Card.jsx'
+ 
+function App() {
+
+  return(
+    <>
+    <Card />
+    <Card />
+    </>
+  )
+}
+
+export default App
